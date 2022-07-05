30df4b0601ab8aae135900379802a54559116848
MinDong <mdskylover@gmail.com>
2022-07-05 13:15:09 +0900

    Remove asyncio module

--- src/downloader.py
+++ src/downloader.py
@@ -1,5 +1,3 @@
-import asyncio
-import time
 from pathlib import Path
 
 import requests
@@ -7,21 +5,18 @@ import requests
 from src.config import load_config
 
 
-async def download_file(data_url: str, filename: str, target_path: Path):
+def download_file(data_url: str, filename: str, target_path: Path):
     r = requests.get(data_url + filename, stream=True, verify=True)
     with open(Path(target_path, filename), "wb") as f:
         f.write(r.content)
 
 
-async def download_raw_data(config: dict[str, str], target_path: Path) -> None:
-    asyncio.gather(
-        download_file(config["url"], config["train_file"], target_path),
-        download_file(config["url"], config["test_file"], target_path),
-    )
+def download_raw_data(config: dict[str, str], target_path: Path) -> None:
+    download_file(config["url"], config["train_file"], target_path),
+    download_file(config["url"], config["test_file"], target_path),
 
 
 if __name__ == "__main__":
-    start = time.perf_counter()
+
     config = load_config()
-    asyncio.run(download_raw_data(config, Path("data")))
-    print(f"Downloaded in {time.perf_counter() - start} seconds")
+    download_raw_data(config, Path("data"))
